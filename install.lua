ab789a19dd3e9e01dc0a30d731ed7c8524a91c77
Dilmohit Singh Bhatha <145561841+dillon-bhatha@users.noreply.github.com>
2024-05-16 09:29:30 +0200

    Add files via upload

---
+++ install.lua
@@ -0,0 +1,24 @@
+local githubUsername = "<dillon-bhatha>"
+local repositoryName = "<minecraft_chest_monitor>"
+local chestMonitorScriptUrl = "https://github.com/dillon-bhatha/minecraft_chest_monitor" .. githubUsername .. "/" .. repositoryName .. "/main/chest_monitor.lua"
+
+
+local response = http.get(chestMonitorScriptUrl)
+if response then
+    local scriptContent = response.readAll()
+    response.close()
+    
+
+    local file = fs.open("chest_monitor.lua", "w")
+    file.write(scriptContent)
+    file.close()
+    
+
+    file = fs.open("startup.lua", "w")
+    file.write("shell.run(\"chest_monitor.lua\")")
+    file.close()
+    
+    print("Chest monitor is geïnstalleerd!")
+else
+    print("Kon het chest monitor script niet downloaden.")
+end
